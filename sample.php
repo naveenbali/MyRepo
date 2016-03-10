1c9bb0952712782ddc217fbffcbf4e5cde1792e9
naveen bali <naveen@balisoftbiz.com>
2016-03-10 17:30:39 +0530

    <b> This is new</b> checkit sample file Issue #458745

---
+++ sample.php
@@ -0,0 +1,151 @@
+ Show any profile for a user 
+
+Post :- http://directory.aussiebizonline.com.au/api/business_profile/encryt(profile_id) 
+
+Post fields – email, token 
+
+bhkjgkjgjh
+
+kgvjvgjgvbj
+
+,bjvbjvbjbv
+ email,token,realName,dob,body,height,hair_color,eye_color,ethnicity,sexuality,tattoos,piercings,anal,in_out_call,outcalldistance,tourRequest,twitter,facebook,instagram,about,notice,betwenBookings,languages,licence_no,cupSize,penis_length,penis_girth,pupbicHair,circumcised,coreService,lat,lng,services,category,smoker,address,hide(for address hide/show),spa,shower,creditCard,atm,parking,massageClients,website
+
+
+ <pre>Array
+(   
+    [realName] => samplename
+    [category] => Array
+        (
+            [0] => 1
+            [1] => 5
+        )
+
+    [token] => P2IPhBNzgYw4B3PLGyaHi1J3IfRNE4iFBJdxKwjw7Cfq1DfGeTxGKAKydWAHQKBu
+    [email] => testsample_user@email.com    
+    [in_out_call] => Array
+        (
+            [0] => incall
+            [1] => outcall
+        )
+
+    [outcalldistance] => 2
+    [address] => Kotara, New South Wales
+    [dob] => 10/3/2016
+    [height] => 156
+    [body] => athletic
+    [hair_color] => brunette
+    [eye_color] => blue
+    [ethnicity] => asian
+    [languages] => Array
+        (
+            [0] => Azerbaijani
+            [1] => Bengali
+        )
+
+    [website] => 
+    [sexuality] => Straight
+    [smoker] => 1
+    [tattoos] => 1
+    [piercings] => 1
+    [anal] => none
+    [tourRequest] => 1
+    [pubicHair] => shaved
+    [cupSize] => e
+    [weekday] => Array
+        (
+            [Monday] => Array
+                (
+                    [on_time] => 0130
+                    [off_time] => 1830
+                )
+
+            [Tuesday] => Array
+                (
+                    [on_time] => 0230
+                    [off_time] => 1800
+                )
+
+            [Thursday] => Array
+                (
+                    [day] => all
+                )
+
+            [Saturday] => Array
+                (
+                    [day] => all
+                )
+
+            [Sunday] => Array
+                (
+                    [on_time] => 0400
+                    [off_time] => 1700
+                )
+
+        )
+
+    [betwenBookings] => 10 minute
+    [notice] => 3 hours
+    [price] => Array
+        (
+            [15 minutes] => Array
+                (
+                    [time] => 15 minute
+                    [incall] => 5
+                    [outcall] => 6
+                )
+
+            [30 minutes] => Array
+                (
+                    [time] => 30 minute
+                    [incall] => 4
+                    [outcall] => 5
+                )
+
+            [45 minutes] => Array
+                (
+                    [time] => 45 minute
+                    [incall] => 1
+                    [outcall] => 3
+                )
+
+            [1 hour] => Array
+                (
+                    [time] => 1 hour
+                    [incall] => 5
+                    [outcall] => 6
+                )
+
+            [Weekend] => Array
+                (
+                    [time] => Weekend
+                    [incall] => 4
+                    [outcall] => 6
+                )
+
+            [Dinner] => Array
+                (
+                    [time] => Dinner date
+                    [incall] => 7
+                    [outcall] => 9
+                )
+
+            [Couples] => Array
+                (
+                    [time] => Couples per hour
+                    [incall] => 4
+                    [outcall] => 9
+                )
+
+        )
+
+    [services] => Array
+        (
+            [0] => 82
+            [1] => 83
+            [2] => 84
+        )
+    [coreService] => 82 
+
+    [terms] => on
+)
